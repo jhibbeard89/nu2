8af4fa1ad4fec6b21c6dbaa61147a2d0200d302a
James Hibbeard <jhibbeard@gmail.com>
2021-08-24 11:54:06 +0200

    Updated bank and insurance tables to link with user and delted stupid options table WHY did we build it

--- db/migrate/20210824094450_create_insurances.rb
+++ db/migrate/20210824094450_create_insurances.rb
@@ -3,7 +3,7 @@ class CreateInsurances < ActiveRecord::Migration[6.1]
     create_table :insurances do |t|
       t.string :name
       t.boolean :private
-
+      t.references :user, null: false, foreign_key: true
       t.timestamps
     end
   end
