687f54d35649ece31303acecda06d82242a889f7
James Hibbeard <jhibbeard@gmail.com>
2021-08-24 12:12:57 +0200

    updated dateTime to datetime in migrations

--- db/migrate/20210824092532_create_way_answers.rb
+++ db/migrate/20210824092532_create_way_answers.rb
@@ -3,10 +3,10 @@ class CreateWayAnswers < ActiveRecord::Migration[6.1]
     create_table :way_answers do |t|
       t.string :employment_status
       t.string :income_range
-      t.dateTime :arrival_date
+      t.datetime :arrival_date
       t.string :address
       t.string :phone_number
-      t.dateTime :date_of_birth
+      t.datetime :date_of_birth
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
