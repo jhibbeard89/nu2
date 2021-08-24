81c4b9711a57a406db6815de73800350251c7407
Martin Runge <martinrunge@Martins-MBP.fritz.box>
2021-08-24 20:24:50 +0200

    Build index and show page, still not finished

--- app/controllers/registration_offices_controller.rb
+++ app/controllers/registration_offices_controller.rb
@@ -2,4 +2,8 @@ class RegistrationOfficesController < ApplicationController
   def index
     @registration_offices = RegistrationOffice.all
   end
+
+  def show
+    @registration_office = RegistrationOffice.find(params[:id])
+  end
 end
