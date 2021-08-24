adb5f3f5258eb474e84725e6f53d30111bbea4c7
James Hibbeard <jhibbeard@gmail.com>
2021-08-24 12:02:22 +0200

    added choices table because we needed it and we were stupid

---
+++ app/models/user_choice.rb
@@ -0,0 +1,4 @@
+class UserChoice < ApplicationRecord
+  belongs_to :bank
+  belongs_to :insurance
+end
--- db/migrate/20210824094426_create_banks.rb
+++ db/migrate/20210824094426_create_banks.rb
@@ -2,7 +2,6 @@ class CreateBanks < ActiveRecord::Migration[6.1]
   def change
     create_table :banks do |t|
       t.string :name
-      t.references :user, null: false, foreign_key: true
       t.timestamps
     end
   end
