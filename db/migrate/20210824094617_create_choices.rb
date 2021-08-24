ff7c6093589132c59570193f133b052919efff33
James Hibbeard <jhibbeard@gmail.com>
2021-08-24 11:47:22 +0200

    updated choices to include a link to banks and insurance tables, created bank and insurance tables

--- db/migrate/20210824094617_create_choices.rb
+++ db/migrate/20210824094617_create_choices.rb
@@ -1,10 +1,9 @@
 class CreateChoices < ActiveRecord::Migration[6.1]
   def change
     create_table :choices do |t|
-      t.string :bank
-      t.string :insurance
       t.references :user, null: false, foreign_key: true
-
+      t.references :bank, null: false, foreign_key: true
+      t.references :insurance, null: false, foreign_key: true
       t.timestamps
     end
   end
