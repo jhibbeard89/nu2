43ac812e86b6e0e8b1d7cc8f33e434a7d48e5bcd
cheyenne simoleit <cheyenne@simoleit.com>
2021-08-25 13:04:17 +0200

    insurance logic complete yaaaay

--- app/controllers/insurances_controller.rb
+++ app/controllers/insurances_controller.rb
@@ -1,6 +1,8 @@
 class InsurancesController < ApplicationController
   def index
     @insurances = Insurance.all
+    @public_insurances = Insurance.where(private: false)
+    @private_insurances = Insurance.where(private: true)
   end
     
   def show
