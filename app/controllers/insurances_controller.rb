91269f1c4fa347704e3f6311c090a3873c167408
cheyenne simoleit <cheyenne@simoleit.com>
2021-08-25 14:53:20 +0200

    bank logic complete yaaaay

--- app/controllers/insurances_controller.rb
+++ app/controllers/insurances_controller.rb
@@ -3,9 +3,5 @@ class InsurancesController < ApplicationController
     @insurances = Insurance.all
     @public_insurances = Insurance.where(private: false)
     @private_insurances = Insurance.where(private: true)
-  end
-    
-  def show
-    @insurance = Insurance.find(params[:id])
-  end
+  end   
 end
