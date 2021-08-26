00dc7bdffc43eefec78689967ae2015ed40d4272
James Hibbeard <jhibbeard@gmail.com>
2021-08-26 12:47:45 +0200

    Updates to the logic within the user bank questions response

--- app/controllers/banks_controller.rb
+++ app/controllers/banks_controller.rb
@@ -1,8 +1,17 @@
 class BanksController < ApplicationController
   def index
     @banks = Bank.all
-    @n26 = Bank.find_by(name: "N26")
-    @deutschebank = Bank.find_by(name: "Deutsche Bank")
-    @commerzbank = Bank.find_by(name: "Commerzbank")
+    @n26 = Bank.find_by(name: 'N26')
+    @deutschebank = Bank.find_by(name: 'Deutsche Bank')
+    @commerzbank = Bank.find_by(name: 'Commerzbank')
+    @user_answers = current_user.questions
+    @german_level = @user_answers.german_level
+    @duration = @user_answers.duration
+
+    if @duration > 12
+      @bank = @german_level == 'Beginner' ? @deutschebank : @commerzbank
+    else
+      @bank = @n26
+    end
   end
 end
