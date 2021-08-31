42a92158c1e65f7a4fc591c93c887eb685e2109c
James Hibbeard <jhibbeard@gmail.com>
2021-08-31 20:01:34 +0200

    The user portal is updatred with main points, just some more styling required

--- app/controllers/journey_controller.rb
+++ app/controllers/journey_controller.rb
@@ -4,12 +4,17 @@ class JourneyController < ApplicationController
     @bank = @user.user_choices.first.bank
     @insurance = @user.user_choices.first.insurance
     @appointment = @user.appointments.first
-    office = @appointment.registration_office
-    @markers = [{
-      lat: office.latitude,
-      lng: office.longitude,
-      info_window: render_to_string(partial: "/registration_offices/info_window",
-      locals: { office: office })
-    }]
+    unless @appointment.nil? || @appointment.registration_office.nil?
+      @booking_date_time = @appointment.date
+      @date = @booking_date_time.strftime("#{@booking_date_time.day.ordinalize} %B")
+      @time = @appointment.date.strftime('%l:%M %p')
+      office = @appointment.registration_office
+      @markers = [{
+        lat: office.latitude,
+        lng: office.longitude,
+        info_window: render_to_string(partial: "/registration_offices/info_window",
+        locals: { office: office })
+      }]
+    end
   end
 end
