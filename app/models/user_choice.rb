d4243715aa2c9e027d81c7f18af7843983a430cb
Christine Chan <christine.u.chan@gmail.com>
2021-09-01 11:30:23 +0200

    add fake documents upload to journey page

--- app/models/user_choice.rb
+++ app/models/user_choice.rb
@@ -2,5 +2,4 @@ class UserChoice < ApplicationRecord
   belongs_to :bank, optional: true
   belongs_to :insurance, optional: true
   belongs_to :user
-  has_many_attached :photos
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_31_151442) do
+ActiveRecord::Schema.define(version: 2021_09_01_090749) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,11 @@ ActiveRecord::Schema.define(version: 2021_08_31_151442) do
 
   create_table "chatrooms", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "documents", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
