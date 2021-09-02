f66f38ab531353289c7ffcc5af8119005a960307
James Hibbeard <jhibbeard@gmail.com>
2021-09-02 12:28:09 +0200

    Bot now adds message when user registers

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,11 @@
 class Message < ApplicationRecord
   belongs_to :chatroom
   belongs_to :user
+
+  def self.welcome_message(user, chatroom)
+    message = Message.new(content: "Hello #{user.first_name}! How can I help you today?")
+    message.user = user
+    message.chatroom = chatroom
+    message.save!
+  end
 end
